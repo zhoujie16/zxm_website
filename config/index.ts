9c282d43aee94ad97274ea2f92777be947171371
zhoujie <zhoujie16m@163.com>
2021-09-16 01:21:10 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,10 @@
-const Config = {
+let Config = {
   blogName: "周小米的博客",
   blogCode: "zxm_blog",
   apiUrl: "http://127.0.0.1:7001",
 };
+if (process.browser) {
+  Config.apiUrl = "https://x.abczzz.cn";
+}
 
 export default Config;
