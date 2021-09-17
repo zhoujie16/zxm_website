07d8c643452579185c74b7906e3680f267625f5e
zhoujie <zhoujie16m@163.com>
2021-09-18 00:31:38 +0800

    1

--- components/BaseHead/index.tsx
+++ components/BaseHead/index.tsx
@@ -35,8 +35,9 @@ const Index = () => {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css"
       />
-      <link rel="stylesheet" href="https://butterfly.js.org/css/index.css" />
+      <link rel="stylesheet" href="/index.css" />
       <script src="/lib/spm.js"></script>
+      <script src="/index.js"></script>
     </Head>
   );
 };
